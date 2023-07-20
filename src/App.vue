09acf1413ac3e051203db24bf7ea4967aaef6806
Daniil Bobrov <allodrmen@yandex.ru>
2023-07-20 15:08:22 +0300

    added layout for gamepopup

--- src/App.vue
+++ src/App.vue
@@ -8,15 +8,7 @@
     </div>
 
     <!-- Container for final message -->
-    <div v-if="false" class="popup-container">
-      <div class="popup">
-        <h2>Поздравляю, вы победили! 😃</h2>
-        <!-- <h2>Вы проиграли. 😕</h2> -->
-        <!-- <h3>...имя: Лидия</h3> -->
-        <button>Сыграть еще раз</button>
-      </div>
-    </div>
-
+    <GamePopup />
     <!-- Notification -->
     <div class="notification-container show">
       <p>Вы уже вводили этот символ</p>
@@ -29,6 +21,7 @@ import GameHeader from "./components/GameHeader.vue";
 import GameFigure from "./components/GameFigure.vue";
 import GameWrongLetters from "./components/GameWrongLetters.vue";
 import GameWord from "./components/GameWord.vue";
+import GamePopup from "./components/GamePopup.vue";
 </script>
 
 <style></style>
