e89cd05be1d9140a8e82401aaaf9d956bfa5e8a8
Daniil Bobrov <allodrmen@yandex.ru>
2023-07-21 16:17:07 +0300

    added showing game word

--- src/components/GameWord.vue
+++ src/components/GameWord.vue
@@ -1,13 +1,23 @@
 <template>
   <div class="word">
-    <span class="letter">л</span>
-    <span class="letter"></span>
-    <span class="letter">д</span>
-    <span class="letter"></span>
-    <span class="letter"></span>
+    <span v-for="(letter, index) in word" :key="index" class="letter">
+      {{ showLetters(letter) }}
+    </span>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { defineProps } from "vue";
+interface Props {
+  word: string;
+  correctLetters: string[];
+}
+
+const props = defineProps<Props>();
+
+const showLetters = (letter: string) => {
+  return props.correctLetters.includes(letter) ? letter : "";
+};
+</script>
 
 <style scoped></style>
