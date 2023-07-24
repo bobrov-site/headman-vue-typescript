63cdc2af66ea2854d394afb3b98d687e74117da2
Daniil Bobrov <allodrmen@yandex.ru>
2023-07-24 11:38:00 +0300

    created useNotification.ts

---
+++ src/composables/useNotification.ts
@@ -0,0 +1,13 @@
+import { ref } from "vue";
+import GameNotification from "../components/GameNotification.vue";
+export const useNotification = () => {
+  const notification = ref<InstanceType<typeof GameNotification> | null>(null);
+  const showNotification = () => {
+    notification.value?.showNotification();
+    setTimeout(() => notification.value?.closeNotification(), 2000);
+  };
+  return {
+    notification,
+    showNotification,
+  };
+};
